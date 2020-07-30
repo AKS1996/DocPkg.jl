598223aac7cf5137e4ec2e2d4c89d8de466ef0ac
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-07-30 12:25:44 +0000

    build based on a099eed

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DocPkg.jl-Documentation","page":"Home","title":"DocPkg.jl Documentation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"func(x)","category":"page"},{"location":"#DocPkg.func-Tuple{Any}","page":"Home","title":"DocPkg.func","text":"func(x)\n\nReturns double the number x plus 1.\n\n\n\n\n\n","category":"method"}]
+}
